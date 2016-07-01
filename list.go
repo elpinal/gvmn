171ab19631d0b59862eb01e5a3520ae5b9981f5b
elpinal <elpinal@users.noreply.github.com>
2016-07-01 18:44:30 +0900

    Add list/use command

--- list.go
+++ list.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
 var cmdList = &Command{
 	Run:       runList,
 	UsageLine: "list ",
@@ -16,6 +25,24 @@ func init() {
 
 // runList executes list command and return exit code.
 func runList(args []string) int {
-
+	current, _ := os.Readlink(filepath.Join(GvmnDir, "versions", "current"))
+	currentVersion := filepath.Base(current)
+	versions, err := ioutil.ReadDir(filepath.Join(GvmnDir, "versions"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed list versions of Go"))
+	}
+	for _, version := range versions {
+		ver := version.Name()
+		if ver == "current" {
+			continue
+		}
+		var mark string
+		if ver == currentVersion {
+			mark = "*"
+		} else {
+			mark = " "
+		}
+		fmt.Println(mark, ver)
+	}
 	return 0
 }
--- use.go
+++ use.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
 var cmdUse = &Command{
 	Run:       runUse,
 	UsageLine: "use ",
@@ -16,6 +24,28 @@ func init() {
 
 // runUse executes use command and return exit code.
 func runUse(args []string) int {
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "gvmn use: no Go version specified")
+		return 1
+	}
+	currentDir := filepath.Join(GvmnDir, "versions", "current")
+	version := args[0]
+	versionsDir := filepath.Join(GvmnDir, "versions", version)
+	if _, err := os.Stat(versionsDir); err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrap(err, "no installed version of Go specified"))
+		return 1
+	}
+	if _, err := os.Stat(currentDir); err == nil {
+		if err := os.RemoveAll(currentDir); err != nil {
+			fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed unuse former version of Go"))
+			return 1
+		}
+	}
+	err := os.Symlink(versionsDir, currentDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed create symbolic link"))
+		return 1
+	}
 
 	return 0
 }
