9594af6980987a501a3498b94b973764df7e3bd9
elpinal <elpinal@users.noreply.github.com>
2016-08-29 18:48:16 +0900

    Add uninstall

--- uninstall.go
+++ uninstall.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 var cmdUninstall = &Command{
 	Run:       runUninstall,
 	UsageLine: "uninstall ",
@@ -14,8 +21,30 @@ func init() {
 	// cmdUninstall.Flag.BoolVar(&flagA, "a", false, "")
 }
 
+// uninstall uninstalls specified versions of Go.
+func uninstall(versions []string) error {
+	for _, version := range versions {
+		dir := filepath.Join(GvmnDir, "versions", version)
+		if !exist(dir) {
+			return fmt.Errorf("no Go version specified")
+		}
+		if err := os.RemoveAll(dir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // runUninstall executes uninstall command and return exit code.
 func runUninstall(args []string) int {
+	if len(args) == 0 {
+		log.Print("gvmn uninstall: no Go versions specified")
+		return 1
+	}
 
+	if err := uninstall(args); err != nil {
+		log.Print(err)
+		return 1
+	}
 	return 0
 }
