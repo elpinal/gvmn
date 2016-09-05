328a7395c44d4c3234f63820c0bb5b600e4bbdcb
elpinal <elpinal@users.noreply.github.com>
2016-09-05 21:47:49 +0900

    Support get multiple versions

--- cmd/gvmn/get.go
+++ cmd/gvmn/get.go
@@ -8,13 +8,13 @@ import (
 
 var cmdGet = &Command{
 	Run:       runGet,
-	UsageLine: "get [-d] version",
+	UsageLine: "get [-d] versions...",
 	Short:     "download and install Go",
-	Long:      `
-Get downloads the specified Go version, and then installs it.
+	Long: `
+Get downloads the specified Go versions, and then installs them.
 
-The -d flag instructs get to stop after downloading the Go version; that is,
-it instructs get not to install the Go version.
+The -d flag instructs get to stop after downloading the Go versions; that is,
+it instructs get not to install the Go versions.
 `,
 }
 
@@ -27,32 +27,35 @@ func init() {
 // runGet executes get command and return exit code.
 func runGet(cmd *Command, args []string) int {
 	if len(args) == 0 {
-		log.Print("gvmn get: no go version specified")
+		log.Print("gvmn get: no go versions specified")
 		return 1
 	}
 
-	version := args[0]
-	if version == "latest" {
-		latest, err := gvmn.LatestTag()
-		if err != nil {
+	for _, version := range args {
+		if version == "latest" {
+			latest, err := gvmn.LatestTag()
+			if err != nil {
+				log.Print(err)
+				return 1
+			}
+			version = latest
+		}
+
+		if err := gvmn.Download(version); err != nil {
 			log.Print(err)
 			return 1
 		}
-		version = latest
-	}
-
-	if err := gvmn.Download(version); err != nil {
-		log.Print(err)
-		return 1
 	}
 
 	if getD {
 		return 0
 	}
 
-	if err := gvmn.Install(version); err != nil {
-		log.Print(err)
-		return 1
+	for _, version := range args {
+		if err := gvmn.Install(version); err != nil {
+			log.Print(err)
+			return 1
+		}
 	}
 
 	return 0
