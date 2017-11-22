40485a08ec2899cc3b517923fd666dbd4d6f93ad
elpinal <elpinal@users.noreply.github.com>
2017-11-22 23:14:24 +0900

    Test list command

--- cmd/gvmn/list_test.go
+++ cmd/gvmn/list_test.go
@@ -1,7 +1,57 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
 
-func TestCmdList(t *testing.T) {
-	// Write your code here
+	"github.com/elpinal/gvmn"
+)
+
+func TestMain(m *testing.M) {
+	dir, err := ioutil.TempDir("", "gvmn")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	gvmn.SetRoot(dir)
+
+	subdir := filepath.Join(dir, "go/go1.7/bin")
+	err = os.MkdirAll(subdir, os.ModePerm)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	f, err := os.Create(filepath.Join(subdir, "go"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestList(t *testing.T) {
+	var buf bytes.Buffer
+	l := lister{out: &buf, err: &buf}
+	code := l.listMain()
+	s := buf.String()
+	if code != 0 {
+		t.Fatalf("listMain = %d; output is: %s", code, s)
+	}
+	if !strings.Contains(s, "Installed:") {
+		t.Fatalf("listMain: output does not contain %q in %q", "Installed:", s)
+	}
 }
